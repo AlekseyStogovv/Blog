505f869d8049368ada777955a678fd0a83178379
AlekseyStogovv <astogovv@gmail.com>
2023-06-26 13:53:57 +0300

    6 day

--- Blog/tests.py
+++ Blog/tests.py
@@ -33,19 +33,10 @@ class ArticlePageTest(TestCase):
 
 class HomePageTest(TestCase):
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
-
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>Сайт Алексея Стогова</title>', html)
-        self.assertIn('<h1>Алексей Стогов</h1>', html)
-        self.assertTrue(html.endswith('</html>'))
+        url = reverse('home_page')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'home_page.html')
 
 
     def test_home_page_displays_correct_articles(self):
